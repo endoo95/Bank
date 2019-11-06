52fa3168d95914c0c3fc9796e0284e45c3b837db
Endoo <endoo95@gmail.com>
2019-11-06 14:39:59 +0100

    Zakończony program bankowy według kursu.
    TODO: Można przerobić na zapis do pliku i usprawnić program.

--- Bank/BankManager.cs
+++ Bank/BankManager.cs
@@ -50,41 +50,52 @@ namespace Bank
                         Console.Clear();
                         Console.WriteLine("Wybrano listę kont klienta!");
                         Console.ReadKey();
+                        ListOfAccounts();
                         break;
                     case 2:
                         Console.Clear();
                         Console.WriteLine("Wybrano dodanie konta rozliczeniowego!");
                         Console.ReadKey();
+                        AddBillingAccount();
                         break;
                     case 3:
                         Console.Clear();
                         Console.WriteLine("Wybrano dodanie konta oszczędnościowego!");
                         Console.ReadKey();
+                        AddSavingsAccount();
                         break;
                     case 4:
                         Console.Clear();
                         Console.WriteLine("Wybrano wpłatę pieniędzy!");
                         Console.ReadKey();
+                        AddMoney();
                         break;
                     case 5:
                         Console.Clear();
                         Console.WriteLine("Wybrano wypłatę pieniędzy!");
                         Console.ReadKey();
+                        TakeMoney();
                         break;
                     case 6:
                         Console.Clear();
                         Console.WriteLine("Wybrano listę klientów!");
                         Console.ReadKey();
+                        GetAllCustomers();
                         break;
                     case 7:
                         Console.Clear();
                         Console.WriteLine("Wybrano listę wszystkich kont!");
                         Console.ReadKey();
+                        GetAllAccounts();
                         break;
                     case 8:
                         Console.Clear();
                         Console.WriteLine("Wybrano zakończenie miesiąca!");
                         Console.ReadKey();
+                        Console.Clear();
+                        EndOfMonth();
+                        break;
+                    case 0:
                         break;
                     default:
                         Console.Clear();
@@ -114,14 +125,141 @@ namespace Bank
         {
             Console.Clear();
             CustomerData data = ReadCustomerData();
-            Console.WriteLine();
-            Console.WriteLine("Konta klienta {0} {1} {2}", data.FirstName, data.LastName, data.PeselNumber);
+            Console.WriteLine("Konta klienta {0} {1} | {2}", data.FirstName, data.LastName, data.PeselNumber);
 
             foreach (Account account in _accountsManager.GetAllAccountsFor(data.FirstName, data.LastName, data.PeselNumber))
             {
                 _printer.Print(account);
             }
             Console.ReadKey();
+        }
+
+        //Przypadek 2 - dodanie konta rozliczeniowego
+        private void AddBillingAccount()
+        {
+            Console.Clear();
+            CustomerData data = ReadCustomerData();
+            Account billingAccount = _accountsManager.CreateBillingAccount(data.FirstName, data.LastName, data.PeselNumber);
+
+            Console.WriteLine("Utworzone nowe konto klienta dla {0} {1}!", data.FirstName, data.LastName);
+            Console.WriteLine();
+            _printer.Print(billingAccount);
+            Console.ReadKey();
+        }
+
+        //Przypadek 3 - dodanie konta oszczędnościowego
+        //TODO: W przyszłości zmienić na dodanie konta z wyborem.
+        private void AddSavingsAccount()
+        {
+            Console.Clear();
+            CustomerData data = ReadCustomerData();
+            Account savingsAccount = _accountsManager.CreateSavingsAccount(data.FirstName, data.LastName, data.PeselNumber);
+
+            Console.WriteLine("Utworzone nowe konto klienta dla {0} {1}!", data.FirstName, data.LastName);
+            Console.WriteLine();
+            _printer.Print(savingsAccount);
+            Console.ReadKey();
+        }
+
+        //Przypadek 4 - Wpłata na konto
+        private void AddMoney()
+        {
+            Console.Clear();
+
+            string accountNo;
+            decimal value;
+
+            Console.Write("Podaj numer konta: ");
+            accountNo = Console.ReadLine();
+            Console.Write("Podaj kwotę wpłaty: ");
+            value = decimal.Parse(Console.ReadLine());
+            _accountsManager.AddMoney(accountNo, value);
+
+            Account account = _accountsManager.GetAccount(accountNo);
+            _printer.Print(account);
+            Console.ReadKey();
+        }
+
+        //Przypadek 5 - Wypłata pieniędzy
+        //To samo co z kontami, uprościć
+
+        private void TakeMoney()
+        {
+            Console.Clear();
+
+            string accountNo;
+            decimal value;
+
+            Console.Write("Podaj numer konta: ");
+            accountNo = Console.ReadLine();
+            Console.Write("Podaj kwotę wypłaty: ");
+            value = decimal.Parse(Console.ReadLine());
+            _accountsManager.TakeMoney(accountNo, value);
+
+            Account account = _accountsManager.GetAccount(accountNo);
+            _printer.Print(account);
+            Console.ReadKey();
+        }
+
+        //Przypadek 6 - Wypisanie klientów banku
+        private void GetAllCustomers()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista wszystkich klientów banku: ");
+            foreach(string customer in _accountsManager.ListOfCustomers())
+            {
+                Console.WriteLine(customer);
+            }
+            Console.ReadKey();
+        }
+
+        //Przypadek 7 - Wypisanie wszystkich kont w banku
+        private void GetAllAccounts()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista wszystkich kont w banku: ");
+            foreach(Account account in _accountsManager.GetAllAccounts())
+            {
+                _printer.Print(account);
+            }
+
+            Console.ReadKey();
+        }
+
+        //Przypadek 8 - Koniec miesiąca
+        private void EndOfMonth()
+        {
+            string answer;
+
+            do
+            {
+                Console.Write("Czy na pewno zakończyć miesiąc? (Y/N): ");
+                answer = Console.ReadLine();
+
+                if (answer == "y" || answer == "Y")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Kończę miesiąc!");
+                    _accountsManager.CloseMonth();
+                    Console.ReadKey();
+                    GetAllAccounts();
+                    break;
+                } 
+                else if (answer == "n" || answer == "N")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wychodzę do menu!");
+                    Console.ReadKey();
+                    break;
+                } 
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Podaj prawidłowe dane!");
+                }
+
+
+            } while (!(answer == "y" || answer == "Y" || answer == "n" || answer == "N"));
         }
 
         //FUNKCJE DO PRZYPADKÓW
@@ -138,6 +276,8 @@ namespace Bank
             lastName = Console.ReadLine();
             Console.Write("PESEL: ");
             peselNumber = Console.ReadLine();
+
+            Console.WriteLine();
 
             return new CustomerData(firstName, lastName, peselNumber);
         }
